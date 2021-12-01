6d725d50978cb3c8bd1bf25acaeec57db87815bf
Kamil Niedzielak <kamil.niedzielak.ctr@sabre.com>
2021-12-01 21:33:46 +0100

    Initial commit

---
+++ src/main/java/com/edge1/kamil/rewardapp/RewardAppApplication.java
@@ -0,0 +1,13 @@
+package com.edge1.kamil.rewardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardAppApplication.class, args);
+    }
+
+}
